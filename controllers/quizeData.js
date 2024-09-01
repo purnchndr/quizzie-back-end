206df8807917f2d1360524f80f200296ce18746e
Purnachandra <pratap.careers@gmail.com>
2024-09-01 22:46:57 +0530

    added analytics functions

--- controllers/quizeData.js
+++ controllers/quizeData.js
@@ -57,20 +57,103 @@ const getAllQuizeAnalysis = async (req, res, next) => {
   const id = req.params.id;
   try {
     let quize = await Quize.findById(id);
-    console.log(quize);
-
     if (!quize)
       return res.status(404).json({
         message: 'Quize not found',
         result: false,
       });
+    let quizeData = await QuizeData.find({ quize: id });
+    const analytics = {
+      name: quize.name,
+      createdOn: quize.createdOn,
+      impressions: quize.impressions,
+      type: quize.type,
+    };
+    if (quize.type === 'poll') {
+      analytics.questions = quize.questions.map((c, i) => {
+        const answers = Array.from({ length: 5 }, () => 0);
+        quizeData.forEach((c, j) => {
+          let selected = c.questions[i].selected;
+          selected = selected === -1 ? 4 : selected;
+          answers[selected] = answers[selected] + 1;
+        });
 
-    let allQuizes = await QuizeData.find({ quize: id });
-    let analytics = allQuizes.quizes;
+        let options;
+        if (c.type === 't')
+          options = c.options.map((d, i) => {
+            return { text: d.text, selected: answers[i] };
+          });
+        else if (c.type === 'i')
+          options = c.options.map((d, i) => {
+            return { url: d.url, selected: answers[i] };
+          });
+        else
+          options = c.options.map((d, i) => {
+            return { text: d.text, url: d.url, selected: answers[i] };
+          });
+        return { name: c.name, options, type: c.type };
+      });
+    } else if (quize.type === 'qna') {
+      analytics.questions = quize.questions.map((c, i) => {
+        let currect = 0;
+        let incurrect = 0;
+        quizeData.forEach((d, j) => {
+          if (d.questions[i].currect) currect++;
+          else incurrect++;
+        });
+        const taken = currect + incurrect;
+        const name = c.name;
+        return { name, currect, incurrect, taken };
+        // const incurr = Array.from({ length: 5 }, () => 0);
+        // quizeData.forEach((c, j) => {
+        //   let currect = c.questions[i].currect;
+        //   console.log(currect);
+        //   if (currect) curr[j] = curr[j] + 1;
+        //   else incurr[j] = incurr[j] + 1;
+        // });
+
+        // console.log(curr);
+        // console.log(incurr);
+
+        // let options;
+
+        // if (c.type === 't')
+        //   options = c.options.map((d, i) => {
+        //     return {
+        //       text: d.text,
+        //       currect: curr[i],
+        //       incurrect: incurr[i],
+        //       taken: curr[i] + incurr[i],
+        //     };
+        //   });
+        // else if (c.type === 'i')
+        //   options = c.options.map((d, i) => {
+        //     return {
+        //       url: d.url,
+        //       currect: curr[i],
+        //       incurrect: incurr[i],
+        //       taken: curr[i] + incurr[i],
+        //     };
+        //   });
+        // else
+        //   options = c.options.map((d, i) => {
+        //     return {
+        //       text: d.text,
+        //       url: d.url,
+        //       currect: curr[i],
+        //       incurrect: incurr[i],
+        //       taken: curr[i] + incurr[i],
+        //     };
+        //   });
+        // return { name: c.name, options, type: c.type };
+        // return null;
+      });
+    }
 
     res.status(200).json({
       message: 'Data fetched succesfully',
-      allQuizes,
+      quizeData,
+      analytics,
       result: true,
     });
   } catch (err) {
--- routes/analytics.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authUser = require('./auth');
-const { getDashboard } = require('../controllers/analytics');
-
-router.get('/', authUser, getDashboard);
-
-module.exports = router;
